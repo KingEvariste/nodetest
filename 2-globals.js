c68c4fcb1e397b54aad860bd8dcb22aeccd206c2
KingEvariste <casasjosephm@gmail.com>
2024-07-21 17:19:21 +0800

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,11 @@
+//GLOBALS - NO WINDOW!!!!
+
+// __dirname  - path to current directory
+// __filename - file name
+// require    - function to use modules(CommonJS)
+// module     - info about current module (file)
+// process    - info about env where the program is being executed
+
+console.log(__dirname);
+// spits out the current path in console
+// to run type "node 2-globals.js in terminal"
